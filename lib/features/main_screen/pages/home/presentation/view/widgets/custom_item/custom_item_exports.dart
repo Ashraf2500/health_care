40f6e33b01f03e44d93a3a1f3bc096988d523a94
Ashraf Hatem <ashraf.omaara47@gmail.com>
2024-04-21 03:54:51 +0200

    create folder "custom_item" in home folder and moved any file responsible on creating any item

---
+++ lib/features/main_screen/pages/home/presentation/view/widgets/custom_item/custom_item_exports.dart
@@ -0,0 +1,5 @@
+export 'custom_category_item.dart';
+export 'custom_notification_item.dart';
+export 'Custom_message_item.dart';
+export 'custom_notification_item.dart';
+export 'custom_popular_doctor_item.dart';
